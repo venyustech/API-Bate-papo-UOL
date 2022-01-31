a1bade32e6a6b586549d34c10d71a624d4744fa8
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-01-31 15:50:07 -0300

    add post and get withou validation

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import dotenv from "dotenv";
-import express, { json } from 'express';
+import express, { json } from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br.js";
+// import joi from "joi";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 
@@ -9,34 +13,64 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-const testes = [
-    {
-        id: 1,
-        nome: "bleble",
-        autor: "raul de souza"
-    },
-    {
-        id: 2,
-        nome: "bleble",
-        autor: "raul de souza"
-    },
-
-]
-
-app.get("/livros", (req, res) => {
-    res.send(testes)
-})
-
-app.get("/livros/:id", (req, res) => {
-
-    const livrosFiltrados = testes.find(livro => livro.id === parseInt(req.params.id))
-    res.send(livrosFiltrados)
-})
-app.post("/livros", (req, res) => {
-    testes.push({ id: 4, nome: "blddedasmddse", autor: "3dasomdaksula de souza" });
-    res.send(testes)
-})
-
 app.listen(5000, () => {
     console.log("rodando em https://localhost:5000")
-}) 
+})
+
+app.get("/participants", async (_, res) => {
+    const mongoClient = new MongoClient(process.env.MONGO_URI);
+    try {
+        await mongoClient.connect();
+        const participants = await mongoClient
+            .db("uol-chat")
+            .collection("participants")
+            .find({})
+            .toArray();
+        res.status(200).send(participants);
+        mongoClient.close();
+    } catch (error) {
+        res.send("#01ERROR: get participants");
+        mongoClient.close();
+    }
+});
+
+app.post("/participants", async (req, res) => {
+    const mongoClient = new MongoClient(process.env.MONGO_URI);
+    try {
+        await mongoClient.connect();
+        const userName = await mongoClient
+            .collection("participants")
+            .db("uol-chat")
+            .inserOne({
+                name: req.body.name,
+                lastStatuss: Data.now(),
+            });
+        res.status(201).send(userName);
+
+        mongoClient.close();
+    } catch (error) {
+        res.send("#02ERROR: post participants");
+        mongoClient.close();
+    }
+});
+
+app.post("/messages", async (req, res) => {
+    const mongoClient = new MongoClient(process.env.MONGO_URI);
+    try {
+        await mongoClient.connect();
+        const message = await mongoClient
+            .db("uol-chat")
+            .collection("messages")
+            .insertOne({
+                from: req.headers.user,
+                to: req.body.to,
+                text: req.body.text,
+                type: req.body.type,
+                time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`,
+            });
+        res.status(200).send(message);
+        mongoClient.close();
+    } catch (error) {
+        res.status(400).send("#03ERROR: post messages")
+    }
+});
